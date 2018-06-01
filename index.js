8292e37a51408b4142ec0280ef017d5230ab7fcb
chandana M <chandu11288@gmail.com>
2018-06-01 02:26:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,16 @@ function ternaryCheckCity(city){
   }else{
     return 'No go.';
   }
+
+}
+
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return 'Thank you so much.';
+  }else if(tip === 'not as generous'){
+    return 'Thank you.';
+  }else{
+    return 'Bye.';
+  }
   
 }
