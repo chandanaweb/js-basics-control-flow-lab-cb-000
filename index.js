e5406473e8ddf3588acfc11cdb49b5f5fec5dfe1
chandana M <chandu11288@gmail.com>
2018-06-01 02:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+function scuberGreetingForFeet(num){
+  if(num < 400) {
+    return 'This one is on me!';
+  }
+    else if(num >2000 && num < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    }
+    else{
+      return 'No can do.';
+    }
+}
+
+function ternaryCheckCity(city){
+  if (city === 'NYC')
+  return 'Ok, sounds good.'
+}
